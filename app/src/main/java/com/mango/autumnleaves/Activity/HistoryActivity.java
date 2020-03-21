04f17b02a7f2b8f282645dfebb21870454c84c57
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-03-21 16:41:06 +0700

    Add and retrieve Data History

--- app/src/main/java/com/mango/autumnleaves/Activity/HistoryActivity.java
+++ app/src/main/java/com/mango/autumnleaves/Activity/HistoryActivity.java
@@ -1,5 +1,6 @@
 package com.mango.autumnleaves.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,11 +16,21 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.mango.autumnleaves.R;
 import com.mango.autumnleaves.adapter.HistoryAdapter;
 import com.mango.autumnleaves.adapter.JadwalAdapter;
 import com.mango.autumnleaves.model.History;
 import com.mango.autumnleaves.model.Jadwal;
+import com.mango.autumnleaves.model.User;
 import com.mango.autumnleaves.remote.Koneksi;
 import com.mango.autumnleaves.remote.Volley;
 import com.mango.autumnleaves.util.Util;
@@ -34,14 +45,23 @@ public class HistoryActivity extends AppCompatActivity {
 
     private ArrayList<History> arrayList;
     RecyclerView recyclerView;
+
     private TextView emptyView;
     private String getid;
     private ProgressBar progressBar;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
 
         progressBar = findViewById(R.id.progressBarHistory);
         recyclerView = findViewById(R.id.rvHistory);
@@ -51,52 +71,103 @@ public class HistoryActivity extends AppCompatActivity {
         arrayList = new ArrayList<>();
 
         getid = Util.getData("account", "id", getApplicationContext());
-        getHistory();
+        showHistory();
 
         progressBar.setVisibility(View.VISIBLE);
 
     }
+//    private void getHistory() {
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, Koneksi.presensi, null,
+//                new Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        progressBar.setVisibility(View.GONE);
+//                        try {
+//                            JSONArray jsonArray = response.getJSONArray("presensi");
+//                            for (int i = 0; i < jsonArray.length(); i++) {
+//                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+//                                History history = new History();
+//                                history.setId_presensi(jsonObject.getInt("id_presensi"));
+//                                history.setId_mahasiswa(jsonObject.getInt("id_mahasiswa"));
+//                                history.setRuangan(jsonObject.getString("ruangan"));
+//                                history.setTanggal(jsonObject.getString("tanggal"));
+//                                history.setWaktu(jsonObject.getString("waktu"));
+//                                history.setMatakuliah(jsonObject.getString("matakuliah"));
+//
+//                                String id = String.valueOf(history.getId_mahasiswa());
+//
+//                                if (getid.equals(id)){
+//                                    arrayList.add(history);
+//                                }
+//
+//                                setuprecyclerview(arrayList);
+//                            }
+//                        } catch (JSONException e) {
+//                                e.printStackTrace();
+//                        }
+//
+////                        Log.d("jsonhistory", response.toString());
+//                    }
+//                }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//
+//            }
+//        });
+//        Volley.getInstance().addToRequestQueue(jsonObjectRequest);
+//    }
+    private void showHistory(){
+    String idUser;
+    idUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    // doccumentsnapshoot untuk mendapatkan dokumen secara spesifik
+    DocumentReference docRef = firebaseFirestore.collection("user").document(idUser);
+    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        @Override
+        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+            if (task.isSuccessful()) {
+                progressBar.setVisibility(View.GONE);
+                DocumentSnapshot document = task.getResult();
+                if (document.exists()) {
+                    User user = new User();
+                    user.setJurusan(document.getString("jurusan"));
+                    user.setKode_kelas(document.getString("kode_kelas"));
 
-    private void getHistory() {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, Koneksi.presensi, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        progressBar.setVisibility(View.GONE);
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("presensi");
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                History history = new History();
-                                history.setId_presensi(jsonObject.getInt("id_presensi"));
-                                history.setId_mahasiswa(jsonObject.getInt("id_mahasiswa"));
-                                history.setRuangan(jsonObject.getString("ruangan"));
-                                history.setTanggal(jsonObject.getString("tanggal"));
-                                history.setWaktu(jsonObject.getString("waktu"));
-                                history.setMatakuliah(jsonObject.getString("matakuliah"));
+                    // Doc Ref Dari user
+                    String jurusanRef = user.getJurusan();
+                    String kelasRef = user.getKode_kelas();
 
-                                String id = String.valueOf(history.getId_mahasiswa());
-
-                                if (getid.equals(id)){
+                    // Querysnapshot untuk mendapatkan semua data dari doccument
+                    firebaseFirestore
+                            .collection("presensi")
+                            .document("kelas")
+                            .collection(kelasRef)
+                            .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    History history = new History();
+                                    history.setMatakuliah(document.getString("matakuliah"));
+                                    history.setRuangan(document.getString("ruangan"));
+                                    history.setTanggal(document.getString("waktu"));
+                                    history.setWaktu(document.getString("jam"));
                                     arrayList.add(history);
                                 }
-
-                                setuprecyclerview(arrayList);
+                            } else {
+                                Log.d("tes", "Error getting documents: ", task.getException());
                             }
-                        } catch (JSONException e) {
-                                e.printStackTrace();
+                            setuprecyclerview(arrayList);
                         }
-
-//                        Log.d("jsonhistory", response.toString());
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
+                    });
+                } else {
+                    Log.d("gagal", "Documment tidak ada");
+                }
+            } else {
+                Log.d("gagal", "gagal", task.getException());
             }
-        });
-        Volley.getInstance().addToRequestQueue(jsonObjectRequest);
-    }
+        }
+    });
+}
     private void setuprecyclerview(ArrayList<History> arrayList) {
         HistoryAdapter historyAdapter = new HistoryAdapter(this,arrayList);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
--- app/src/main/java/com/mango/autumnleaves/beacon/ProximityContentAdapter.java
+++ app/src/main/java/com/mango/autumnleaves/beacon/ProximityContentAdapter.java
@@ -1,7 +1,7 @@
 package com.mango.autumnleaves.beacon;
 
 import android.content.Context;
-import android.content.Intent;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,52 +12,42 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.mango.autumnleaves.R;
 import com.mango.autumnleaves.model.Jadwal;
-import com.mango.autumnleaves.model.Presensi;
-import com.mango.autumnleaves.remote.Koneksi;
-import com.mango.autumnleaves.remote.Volley;
-import com.mango.autumnleaves.util.EstimoteUtils;
-import com.mango.autumnleaves.util.NotificationHelper;
-import com.mango.autumnleaves.util.Util;
-import com.pranavpandey.android.dynamic.toasts.DynamicToast;
-import com.tapadoo.alerter.Alerter;
+import com.mango.autumnleaves.model.User;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 public class ProximityContentAdapter extends BaseAdapter {
 
     private Context context;
-    private String getwaktu , gettanggal ,getjam;
-    private String mKelas , mMatkul;
-    private DatabaseReference databaseReference;
-    private String getid;
-    private String hari, timeNow;
+    private String getjam;
+    private String hari, waktusekarang;
+    private String dataDosen,dataMatakuliah,dataWaktuMulai,dataWaktuSelesai,dataRuangan,dataNodata;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore firebaseFirestore;
 
     public ProximityContentAdapter(Context context) {
         this.context = context;
@@ -97,71 +87,177 @@ public class ProximityContentAdapter extends BaseAdapter {
 
         TextView kelas = convertView.findViewById(R.id.beacon_kelas);
         TextView idbeacon = convertView.findViewById(R.id.beacon_matakuliah);
-        TextView waktu = convertView.findViewById(R.id.tVwaktu);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        // Set Content Beacon
         ProximityContent content = nearbyContent.get(position);
         kelas.setText(content.getKelas());
         idbeacon.setText(content.getIdbeacon());
 
-        // Get Waktu Dari Method Untuk Di tampilkan DI card
-        // Waktu Terpisah dari model
-        waktu();
-        waktu.setText(getwaktu);
-
-        // Test Button
-        Button presensiButton = convertView.findViewById(R.id.button_presensi);
-
-        presensiButton.setOnClickListener(new View.OnClickListener() {
+        // Button Presensi
+        Button Presensi = convertView.findViewById(R.id.button_presensi);
+        Presensi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ProximityContent data = nearbyContent.get(position);
-                getid = Util.getData("account", "id", context);
-                StringRequest stringRequest = new StringRequest(Request.Method.POST, Koneksi.presensi_post, new Response.Listener<String>() {
+                // Get Waktu
+                Date date = Calendar.getInstance().getTime();
+                String tanggal = (String) android.text.format.DateFormat.format("d",   date); // 20
+                String monthNumber  = (String) android.text.format.DateFormat.format("M",   date); // 06
+                String year         = (String) DateFormat.format("yyyy", date); // 2013
+                Calendar calendar = Calendar.getInstance();
+                SimpleDateFormat jam = new SimpleDateFormat("kk:mm");
+
+                int month = Integer.parseInt(monthNumber);
+                String bulan = null;
+
+                if (month == 1){
+                    bulan = "Januari";
+                }else if (month == 2){
+                    bulan = "Februari";
+                }else if (month == 3){
+                    bulan = "Maret";
+                }else if (month == 4){
+                    bulan = "April";
+                }else if (month == 5){
+                    bulan = "Mei";
+                }else if (month == 6){
+                    bulan = "Juni";
+                }else if (month == 7){
+                    bulan = "Juli";
+                }else if (month == 8){
+                    bulan = "Agustus";
+                }else if (month == 9){
+                    bulan = "September";
+                }else if (month == 10){
+                    bulan = "Oktober";
+                }else if (month == 11){
+                    bulan = "November";
+                }else if (month == 12){
+                    bulan = "Desember";
+                }
+                String formatWaktuFix = hari + ", "+tanggal+" "+bulan+" "+year;
+                getjam = jam.format(calendar.getTime());
+
+                String idUser;
+                idUser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                // doccumentsnapshoot untuk mendapatkan dokumen user secara spesifik
+                DocumentReference docRef = firebaseFirestore.collection("user").document(idUser);
+                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
                     @Override
-                    public void onResponse(String response) {
-                        Log.d("Respone", response);
-                        DynamicToast.makeSuccess(context,"Presensi Berhasil").show();
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                User user = new User();
+                                user.setNama(document.getString("nama"));
+                                user.setJurusan(document.getString("jurusan"));
+                                user.setKode_kelas(document.getString("kode_kelas"));
+
+                                // Doc Ref Dari user
+                                String jurusanRef = user.getJurusan();
+                                String kelasRef = user.getKode_kelas();
+                                String nama_mhs = user.getNama();
+
+                                getNamaHari();
+                                firebaseFirestore
+                                        .collection("prodi")
+                                        .document(jurusanRef)
+                                        .collection("kelas")
+                                        .document(kelasRef)
+                                        .collection("jadwal")
+                                        .whereEqualTo("hari",hari).whereLessThan("waktu_mulai",waktusekarang)
+                                        .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                            @Override
+                                            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                                                if (e != null) {
+                                                    Log.w("TAGQUERYSNAPHSOT", "Listen failed.", e);
+                                                    return;
+                                                }
+                                                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots){
+                                                    Jadwal jadwal = new Jadwal();
+                                                    jadwal.setHari(documentSnapshot.getString("hari"));
+                                                    jadwal.setMatakuliah(documentSnapshot.getString("matakuliah"));
+                                                    jadwal.setDosen(documentSnapshot.getString("dosen"));
+                                                    jadwal.setRuangan(documentSnapshot.getString("ruangan"));
+                                                    jadwal.setWaktu_mulai(documentSnapshot.getString("waktu_mulai"));
+                                                    jadwal.setWaktu_selesai(documentSnapshot.getString("waktu_selesai"));
+
+                                                    dataMatakuliah = jadwal.getMatakuliah();
+                                                    dataRuangan = jadwal.getRuangan();
+                                                }
+
+                                                Map<String, Object> data = new HashMap<>();
+                                                data.put("nama", nama_mhs);
+                                                data.put("kelas", content.getKelas());
+                                                data.put("matakuliah", dataMatakuliah);
+                                                data.put("ruangan", dataMatakuliah);
+                                                data.put("jam", getjam);
+                                                data.put("waktu", formatWaktuFix);
+
+                                                firebaseFirestore
+                                                        .collection("presensi")
+                                                        .document("kelas")
+                                                        .collection(kelasRef)
+                                                        .add(data)
+                                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                                            @Override
+                                                            public void onSuccess(DocumentReference documentReference) {
+                                                                Toast.makeText(context,"Data Masuk",Toast.LENGTH_LONG).show();
+                                                            }
+                                                        });
+                                            }
+                                        });
+                            } else {
+                                Log.d("TAG", "Documment tidak ada");
+                            }
+                        } else {
+                            Log.d("TAG", "gagal", task.getException());
+                        }
                     }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        DynamicToast.makeError(context,"Gagal presensi").show();
-                    }
-                }){
-                    @Override
-                    protected Map<String, String> getParams() throws AuthFailureError {
-                        Map<String, String> params = new HashMap<>();
-                        params.put("id_mahasiswa", getid);
-                        params.put("waktu", getwaktu);
-                        params.put("tanggal", gettanggal);
-                        params.put("ruangan", data.getKelas());
-                        params.put("matakuliah", data.getKelas());
-                        return params;
-                    }
-                };
-                Volley.getInstance().addToRequestQueue(stringRequest);
+                });
             }
         });
+
+//        presensiButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                ProximityContent data = nearbyContent.get(position);
+//                getid = Util.getData("account", "id", context);
+//                StringRequest stringRequest = new StringRequest(Request.Method.POST, Koneksi.presensi_post, new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        Log.d("Respone", response);
+//                        DynamicToast.makeSuccess(context,"Presensi Berhasil").show();
+//                    }
+//                }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        DynamicToast.makeError(context,"Gagal presensi").show();
+//                    }
+//                }){
+//                    @Override
+//                    protected Map<String, String> getParams() throws AuthFailureError {
+//                        Map<String, String> params = new HashMap<>();
+//                        params.put("id_mahasiswa", getid);
+//                        params.put("waktu", getwaktu);
+//                        params.put("tanggal", gettanggal);
+//                        params.put("ruangan", data.getKelas());
+//                        params.put("matakuliah", data.getKelas());
+//                        return params;
+//                    }
+//                };
+//                Volley.getInstance().addToRequestQueue(stringRequest);
+//            }
+//        });
         return convertView;
     }
 
-    // Method Get Waktu
-    public void waktu(){
-
-        Calendar calendar = Calendar.getInstance();
-        DateFormat date = new SimpleDateFormat("dd-MM-yyyy");
-        DateFormat time = new SimpleDateFormat("kk:mm");
-        DateFormat jam = new SimpleDateFormat("kk");
-        DateFormat bulan = new SimpleDateFormat("M");
-
-        getwaktu = time.format(calendar.getTime());
-        gettanggal = date.format(calendar.getTime());
-        getjam = jam.format(calendar.getTime());
-
-    }
     private void getNamaHari() {
         Date dateNow = Calendar.getInstance().getTime();
-        timeNow = (String) android.text.format.DateFormat.format("HH:mm", dateNow);
+        waktusekarang = (String) android.text.format.DateFormat.format("HH:mm", dateNow);
         hari = (String) android.text.format.DateFormat.format("EEEE", dateNow);
 
         if (hari.equalsIgnoreCase("sunday")) {
