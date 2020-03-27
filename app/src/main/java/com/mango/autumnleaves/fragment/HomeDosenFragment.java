abcd9a5c1528feb4e6bddb218334485231ed98c5
febbydahlan034@gmail.com <33033464+febydahlan@users.noreply.github.com>
2020-03-28 00:32:46 +0800

    Menampilkan Jadwal Hari ini pada layout dosen

--- app/src/main/java/com/mango/autumnleaves/fragment/HomeDosenFragment.java
+++ app/src/main/java/com/mango/autumnleaves/fragment/HomeDosenFragment.java
@@ -5,20 +5,49 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.auth.User;
 import com.mango.autumnleaves.R;
 import com.mango.autumnleaves.activity.base.BaseFragment;
+import com.mango.autumnleaves.adapter.adaptermahasiswa.JadwalAdapter;
+import com.mango.autumnleaves.adapterdosen.JadwalDosenAdapter;
+import com.mango.autumnleaves.model.Jadwal;
+import com.mango.autumnleaves.model.UserDosen;
+import com.mango.autumnleaves.model.UserMahasiswa;
+import com.mango.autumnleaves.util.NotificationHelper;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeDosenFragment extends BaseFragment {
+
+    private ArrayList<Jadwal> arrayList;
+    private RecyclerView recyclerView;
+    private String hari, waktusekarang;
+    private FirebaseUser firebaseUser;
 
     public HomeDosenFragment() {
         // Required empty public constructor
@@ -29,6 +58,136 @@ public class HomeDosenFragment extends BaseFragment {
         return inflater.inflate(R.layout.fragment_home_dosen, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.rv_jadwal_dosen);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        arrayList = new ArrayList<>();
+
+
+        showJadwal();
+        getNamaHari();
+        getWaktuSekarang();
+    }
+
+    // Show Jadwal
+    private void showJadwal() {
+        // doccumentsnapshoot untuk mendapatkan dokumen secara spesifik
+        DocumentReference docRef = firebaseFirestore.collection("user").document(getFirebaseUserId());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+
+                        UserDosen userDosen = new UserDosen();
+                        userDosen.setJurusan(document.getString("jurusan"));
+                        userDosen.setNip(document.getString("nip"));
+
+                        // Doc Ref Dari user
+                        String nipRef = userDosen.getNip();
+
+                        // Querysnapshot untuk mendapatkan semua data dari doccument
+                        firebaseFirestore
+                                .collection("jadwalDosen")
+                                .document(nipRef)
+                                .collection("jadwal")
+                                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        Jadwal jadwal = new Jadwal();
+                                        jadwal.setHari(document.getString("hari"));
+                                        jadwal.setMatakuliah(document.getString("matakuliah"));
+                                        jadwal.setDosen(document.getString("dosen"));
+                                        jadwal.setRuangan(document.getString("ruangan"));
+                                        jadwal.setWaktu_mulai(document.getString("waktu_mulai"));
+                                        jadwal.setWaktu_selesai(document.getString("waktu_selesai"));
+                                        arrayList.add(jadwal);
+                                    }
+                                } else {
+                                    Log.d("tes", "Error getting documents: ", task.getException());
+                                }
+                                setuprecyclerView(arrayList);
+                            }
+                        });
+                    } else {
+                        Log.d("gagal", "Documment tidak ada");
+                    }
+                } else {
+                    Log.d("gagal", "gagal", task.getException());
+                }
+            }
+        });
+    }
+    private void setuprecyclerView(ArrayList<Jadwal> arrayList) {
+        JadwalDosenAdapter dosenAdapter = new JadwalDosenAdapter(getContext(), arrayList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(dosenAdapter);
+    }
+
+    // Refferensi Waktu
+    private void getWaktuSekarang() {
+        Date date = Calendar.getInstance().getTime();
+        String tanggal = (String) DateFormat.format("d", date); // 20
+        String monthNumber = (String) DateFormat.format("M", date); // 06
+        String year = (String) DateFormat.format("yyyy", date); // 2013
+
+        int month = Integer.parseInt(monthNumber);
+        String bulan = null;
+
+        if (month == 1) {
+            bulan = "Januari";
+        } else if (month == 2) {
+            bulan = "Februari";
+        } else if (month == 3) {
+            bulan = "Maret";
+        } else if (month == 4) {
+            bulan = "April";
+        } else if (month == 5) {
+            bulan = "Mei";
+        } else if (month == 6) {
+            bulan = "Juni";
+        } else if (month == 7) {
+            bulan = "Juli";
+        } else if (month == 8) {
+            bulan = "Agustus";
+        } else if (month == 9) {
+            bulan = "September";
+        } else if (month == 10) {
+            bulan = "Oktober";
+        } else if (month == 11) {
+            bulan = "November";
+        } else if (month == 12) {
+            bulan = "Desember";
+        }
+        String formatFix = hari + ", " + tanggal + " " + bulan + " " + year;
+    }
+    private void getNamaHari() {
+        Date dateNow = Calendar.getInstance().getTime();
+        waktusekarang = (String) android.text.format.DateFormat.format("HH:mm", dateNow);
+        hari = (String) android.text.format.DateFormat.format("EEEE", dateNow);
+        if (hari.equalsIgnoreCase("sunday")) {
+            hari = "minggu";
+        } else if (hari.equalsIgnoreCase("monday")) {
+            hari = "senin";
+        } else if (hari.equalsIgnoreCase("tuesday")) {
+            hari = "selasa";
+        } else if (hari.equalsIgnoreCase("wednesday")) {
+            hari = "rabu";
+        } else if (hari.equalsIgnoreCase("thursday")) {
+            hari = "kamis";
+        } else if (hari.equalsIgnoreCase("friday")) {
+            hari = "jumat";
+        } else if (hari.equalsIgnoreCase("saturday")) {
+            hari = "sabtu";
+        }
+    }
 }
 
 
