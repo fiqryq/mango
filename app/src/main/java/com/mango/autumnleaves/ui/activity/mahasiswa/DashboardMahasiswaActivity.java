7dba685f9fb373f485f343ef459eb92f625acd2a
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-05-05 17:50:41 +0700

    delete some usable code

--- app/src/main/java/com/mango/autumnleaves/ui/activity/mahasiswa/DashboardMahasiswaActivity.java
+++ app/src/main/java/com/mango/autumnleaves/ui/activity/mahasiswa/DashboardMahasiswaActivity.java
@@ -202,66 +202,6 @@ public class DashboardMahasiswaActivity extends BaseActivity {
             proximityContentManager.stop();
     }
 
-    // Untuk Get Tanggal Hari ini
-    private void getWaktuSekarang() {
-        Date date = Calendar.getInstance().getTime();
-        String tanggal = (String) android.text.format.DateFormat.format("d", date);
-        String mBulan = (String) android.text.format.DateFormat.format("M", date);
-        String tahun = (String) android.text.format.DateFormat.format("yyyy", date);
-
-        int month = Integer.parseInt(mBulan);
-        String bulan = null;
-
-        if (month == 1) {
-            bulan = "Januari";
-        } else if (month == 2) {
-            bulan = "Februari";
-        } else if (month == 3) {
-            bulan = "Maret";
-        } else if (month == 4) {
-            bulan = "April";
-        } else if (month == 5) {
-            bulan = "Mei";
-        } else if (month == 6) {
-            bulan = "Juni";
-        } else if (month == 7) {
-            bulan = "Juli";
-        } else if (month == 8) {
-            bulan = "Agustus";
-        } else if (month == 9) {
-            bulan = "September";
-        } else if (month == 10) {
-            bulan = "Oktober";
-        } else if (month == 11) {
-            bulan = "November";
-        } else if (month == 12) {
-            bulan = "Desember";
-        }
-
-        String sekarang = hari + ", " + tanggal + " " + bulan + " " + tahun;
-        tvHariIni.setText(String.valueOf(sekarang));
-    }
-
-    private void getNamaHari() {
-        Date dateNow = Calendar.getInstance().getTime();
-        timeNow = (String) android.text.format.DateFormat.format("HH:mm", dateNow);
-        hari = (String) DateFormat.format("EEEE", dateNow);
-        if (hari.equalsIgnoreCase("sunday")) {
-            hari = "minggu";
-        } else if (hari.equalsIgnoreCase("monday")) {
-            hari = "senin";
-        } else if (hari.equalsIgnoreCase("tuesday")) {
-            hari = "selasa";
-        } else if (hari.equalsIgnoreCase("wednesday")) {
-            hari = "rabu";
-        } else if (hari.equalsIgnoreCase("thursday")) {
-            hari = "kamis";
-        } else if (hari.equalsIgnoreCase("friday")) {
-            hari = "jumat";
-        } else if (hari.equalsIgnoreCase("saturday")) {
-            hari = "sabtu";
-        }
-    }
     private boolean isConnected() {
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
